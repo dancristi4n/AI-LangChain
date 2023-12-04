711aab3ecc1229a8b7b449561b9997d49bb86a2c
dancristi4n <danilo.ndos007@gmail.com>
2023-12-04 11:49:20 -0300

    Delete constants.py

--- constants.py
+++
@@ -1,3 +0,0 @@
-# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
-# and rename this file to constants.py.
-APIKEY = "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"
