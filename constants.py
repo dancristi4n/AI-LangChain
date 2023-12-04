448a895b0cb1bc9621452e3b62f0251b80820c9b
danilo.soares <danilo@md2net.com.br>
2023-12-04 11:41:27 -0300

    commit inicial

---
+++ constants.py
@@ -0,0 +1,3 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+# and rename this file to constants.py.
+APIKEY = "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"
